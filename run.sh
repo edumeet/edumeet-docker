90f474eecfb20c4586a63e77151ff399f0b8c6a1
MÉSZÁROS Mihály <misi@majd.eu>
2019-02-05 12:22:59 +0100

    Add logo placeholder

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker run \
 	-v ${PWD}/configs/app:${BASEDIR}/${MM}/app/config \
 	-v ${PWD}/configs/server:${BASEDIR}/${MM}/server/config \
 	-v ${PWD}/certs:${BASEDIR}/${MM}/server/certs \
+	-v ${PWD}/logo/logo.svg:${BASEDIR}/${MM}/app/resources/images/logo.svg \
 	-e BASEDIR=${BASEDIR} -e MM=${MM} \
 	--network host \
 	--detach \
