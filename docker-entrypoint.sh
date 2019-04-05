6a6024baaee42ab6843235aae630a55add5b3a1e
Mészáros Mihály <misi@majd.eu>
2019-04-05 14:45:54 +0200

    Remove gulp distwatch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,6 @@
 echo "Container starting up..." 
 set -e 
 
-#run dist-watch
-cd ${BASEDIR}/${MM}/app
-nohup sh -c "gulp dist-watch" &
-
 cd ${BASEDIR}/${MM}/server
 node ${BASEDIR}/${MM}/server/server.js
 
