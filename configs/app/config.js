0083043a7ab239f074324a5381f191f7cfe8599c
Mészáros Mihály <misi@majd.eu>
2019-05-22 21:20:04 +0200

    fix typo turn url

--- configs/app/config.js
+++ configs/app/config.js
@@ -6,7 +6,7 @@ var config =
 	turnServers     : [
 		{
 			urls : [
-				'turn:turn.example.com:443?transport=tcp'
+				'turn:example.com:443?transport=tcp'
 			],
 			username   : 'example',
 			credential : 'example'
