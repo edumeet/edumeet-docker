2052cf7b9bd12aa0bf5f26f228660347e6bd89a8
Mészáros Mihály <misi@majd.eu>
2019-06-04 10:39:31 +0200

    v3 config changes

--- configs/server/config.js
+++ configs/server/config.js
@@ -33,60 +33,62 @@ module.exports =
 	// Any http request is redirected to https.
 	// Listening port for http server.
 	listeningRedirectPort : 80,
-	// STUN/TURN
+	// Mediasoup settings
 	mediasoup             :
 	{
-		// mediasoup Server settings.
-		logLevel : 'warn',
-		logTags  :
-		[
-			'info',
-			'ice',
-			'dtls',
-			'rtp',
-			'srtp',
-			'rtcp',
-			'rbe',
-			'rtx'
-		],
-		rtcIPv4          : true,
-		rtcIPv6          : true,
-		rtcAnnouncedIPv4 : null,
-		rtcAnnouncedIPv6 : null,
-		rtcMinPort       : 40000,
-		rtcMaxPort       : 49999,
-		// mediasoup Room codecs.
-		mediaCodecs      :
-		[
-			{
-				kind       : 'audio',
-				name       : 'opus',
-				clockRate  : 48000,
-				channels   : 2,
-				parameters :
+		numWorkers : Object.keys(os.cpus()).length,
+		// mediasoup Worker settings.
+		worker     :
+		{
+			logLevel : 'warn',
+			logTags  :
+			[
+				'info',
+				'ice',
+				'dtls',
+				'rtp',
+				'srtp',
+				'rtcp'
+			],
+			rtcMinPort : 40000,
+			rtcMaxPort : 49999
+		},
+		// mediasoup Router settings.
+		router :
+		{
+			// Router media codecs.
+			mediaCodecs :
+			[
 				{
-					useinbandfec : 1
-				}
-			},
-			// {
-			// 	kind      : 'video',
-			// 	name      : 'VP8',
-			// 	clockRate : 90000
-			// }
-			{
-				kind       : 'video',
-				name       : 'H264',
-				clockRate  : 90000,
-				parameters :
+					kind      : 'audio',
+					mimeType  : 'audio/opus',
+					clockRate : 48000,
+					channels  : 2
+				},
 				{
-					'packetization-mode'      : 1,
-					'profile-level-id'        : '42e01f',
-					'level-asymmetry-allowed' : 1
+					kind       : 'video',
+					mimeType   : 'video/h264',
+					clockRate  : 90000,
+					parameters :
+					{
+						'packetization-mode'      : 1,
+						'profile-level-id'        : '42e01f',
+						'level-asymmetry-allowed' : 1,
+						'x-google-start-bitrate'  : 1000
+					}
 				}
-			}
-		],
-		// mediasoup per Peer max sending bitrate (in bps).
-		maxBitrate : 500000
+			]
+		},
+		// mediasoup WebRtcTransport settings.
+		webRtcTransport :
+		{
+			listenIps :
+			[
+				{ ip: 0.0.0.0, announcedIp: null },
+				{ ip: ::, announcedIp: null }
+			],
+			maxIncomingBitrate              : 4000000,
+			initialAvailableOutgoingBitrate : 2000000
+		}
 	}
 };
-
