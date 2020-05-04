f16a2af1b6ad397949443fdacf8c5cf99b6a24f4
Mészáros Mihály <misi@majd.eu>
2020-05-04 03:09:21 +0200

    Update according develop config changes

--- configs/app/config.js
+++ configs/app/config.js
@@ -40,13 +40,18 @@ var config =
 	{
 		tcp : true
 	},
-	lastN       : 4,
-	mobileLastN : 1,
-	background  : 'images/background.jpg',
+	defaultLayout : 'democratic', // democratic, filmstrip
+	lastN         : 4,
+	mobileLastN   : 1,
+	// Highest number of speakers user can select
+	maxLastN      : 5,
+	// If truthy, users can NOT change number of speakers visible
+	lockLastN     : false,
+	background    : 'images/background.jpg',
 	// Add file and uncomment for adding logo to appbar
 	// logo       : 'images/logo.svg',
-	title       : 'Multiparty meeting',
-	theme       :
+	title         : 'Multiparty meeting',
+	theme         :
 	{
 		palette :
 		{
