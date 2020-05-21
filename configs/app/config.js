689595a14e0a8740215256b7a45ad9e3b962e510
Mészáros Mihály <misi@majd.eu>
2020-05-21 04:05:15 +0200

    Update configs to upstream

--- configs/app/config.js
+++ configs/app/config.js
@@ -1,9 +1,29 @@
 // eslint-disable-next-line
 var config =
 {
-	loginEnabled     : false,
-	developmentPort  : 3443,
-	productionPort   : 443,
+	loginEnabled    : false,
+	developmentPort : 3443,
+	productionPort  : 443,
+
+	/**
+	 * Supported browsers version 
+	 * in bowser satisfy format.
+	 * See more:
+	 * https://www.npmjs.com/package/bowser#filtering-browsers
+	 * Otherwise you got a unsupported browser page
+	 */
+	supportedBrowsers :
+	{
+		'windows' : {
+			'internet explorer' : '>12',
+			'microsoft edge'    : '>18'
+		},
+		'safari'                       : '>12',
+		'firefox'                      : '>=60',
+		'chrome'                       : '>=74',
+		'opera'                        : '>=62',
+		'samsung internet for android' : '>=11.1.1.52'
+	},
 
 	/**
 	 * If defaultResolution is set, it will override user settings when joining:
@@ -24,6 +44,16 @@ var config =
 		{ scaleResolutionDownBy: 3 },
 		{ scaleResolutionDownBy: 1 }
 	],
+
+	/**
+	 * Alternative simulcast setting:
+	 * [
+	 *   { maxBitRate: 50000 }, 
+	 *	 { maxBitRate: 1000000 },
+	 *	 { maxBitRate: 4800000 }
+	 *],
+	 **/
+
 	/**
 	 * White listing browsers that support audio output device selection.
 	 * It is not yet fully implemented in Firefox.
@@ -35,23 +65,59 @@ var config =
 		'opera'
 	],
 	// Socket.io request timeout
-	requestTimeout   : 10000,
+	requestTimeout   : 20000,
+	requestRetries   : 3,
 	transportOptions :
 	{
 		tcp : true
 	},
-	defaultLayout : 'democratic', // democratic, filmstrip
-	lastN         : 4,
-	mobileLastN   : 1,
-	// Highest number of speakers user can select
-	maxLastN      : 5,
+	defaultAudio :
+	{
+		sampleRate        : 48000,
+		channelCount      : 1,
+		volume            : 1.0,
+		autoGainControl   : true,
+		echoCancellation  : true,
+		noiseSuppression  : true,
+		voiceActivityMute : false,
+		sampleSize        : 16
+	},
+
+	/**
+	 * Set max number participants in one room that join 
+	 * unmuted. Next participant will join automatically muted
+	 * Default value is 4
+	 * 
+	 * Set it to 0 to disable auto mute functionality, 
+	 * but use it with caution
+	 * full mesh audio strongly decrease room capacity! 
+	 */
+	autoMuteThreshold    : 4,
+	background           : 'images/background.jpg',
+	defaultLayout        : 'democratic', // democratic, filmstrip
+	// If true, will show media control buttons in separate
+	// control bar, not in the ME container.
+	buttonControlBar     : false,
+	// If false, will push videos away to make room for side
+	// drawer. If true, will overlay side drawer over videos
+	drawerOverlayed      : true,
+	// Position of notifications
+	notificationPosition : 'right',
+	// Timeout for autohiding topbar and button control bar
+	hideTimeout          : 3000,
+	// max number of participant that will be visible in 
+	// as speaker
+	lastN                : 4,
+	mobileLastN          : 1,
+	// Highest number of lastN the user can select manually in 
+	// userinteface
+	maxLastN             : 10,
 	// If truthy, users can NOT change number of speakers visible
-	lockLastN     : false,
-	background    : 'images/background.jpg',
+	lockLastN            : false,
 	// Add file and uncomment for adding logo to appbar
 	// logo       : 'images/logo.svg',
-	title         : 'Multiparty meeting',
-	theme         :
+	title                : 'Multiparty meeting',
+	theme                :
 	{
 		palette :
 		{
