48f2c40fa7eec8d2c545bad0afd5cabf99c59884
Mészáros Mihály <misi@majd.eu>
2021-10-19 11:54:42 +0200

    No longer we need rebuild in entrypoint so removed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,11 +10,6 @@ set -e
 
 cd ${BASEDIR}/${EDUMEET}/server
 
-if [[ ! -e ${BASEDIR}/${EDUMEET}/server/dist/config/config.js ]] || [[ $(head -1 ${BASEDIR}/${EDUMEET}/server/config/config.js) != $(head -1 ${BASEDIR}/${EDUMEET}/server/dist/config/config.js) ]] 
-  then
-      echo "Rebuilding configuration..."
-      npm run build
-fi
 
 node ${BASEDIR}/${EDUMEET}/server/dist/server.js
 
