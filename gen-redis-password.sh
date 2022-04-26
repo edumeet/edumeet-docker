0c97d24dc855dc9582ff7178e0e1fe8f9cb22b2e
sd4v1d <sasa.davidovic@ri.carnet.hr>
2022-04-26 10:24:48 +0200

    Updating Redis password setup process

--- gen-redis-password.sh
+++ gen-redis-password.sh
@@ -5,14 +5,25 @@ echo "Generated Redis password: ${REDIS_PASSWORD}"
 REDIS_PASSWORD=${REDIS_PASSWORD//\//\\/}
 
 echo "setting Redis password in ./configs/redis/redis.conf"
-sed -i -r "s/requirepass\ .*/requirepass\ ${REDIS_PASSWORD}/" configs/redis/redis.conf
+sed -i -r "s/^requirepass\ .*/requirepass\ ${REDIS_PASSWORD}/" configs/redis/redis.conf
 
 for fileType in {json,yaml,toml}
 do
   if [ -f ./configs/server/config.${fileType} ]
   then
+    case ${fileType} in
+      json)
+        REGEX='^.*\"password\"\ :'
+        ;;
+      yaml)
+        REGEX='^.*password:'
+        ;;
+      toml)
+        REGEX='^.*password\ ='
+        ;;
+    esac
     echo "setting Redis password in ./configs/server/config.${fileType}"
-    sed -i -r "s/_REDIS_PASSWORD_/${REDIS_PASSWORD}/" configs/server/config.${fileType}
+    sed -i -r "s/(${REGEX}).*/\1 \"${REDIS_PASSWORD}\"/" configs/server/config.${fileType}
   fi
 done
 
