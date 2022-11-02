df734f639d8c333425b1c5bf941eff1fb41b3e3e
Rémai Gábor <remaigabor@gmail.com>
2022-11-02 15:31:21 +0100

    cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 echo "Container starting up..." 
 set -e 
-cd /opt/edumeet-room-server/
-#node /opt/edumeet-room-server/dist/server.js
-yarn start
+node /opt/edumeet/dist/server.js
+#yarn start
 exec "$@"
