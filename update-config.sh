236c1178ff0d2eab71c58e49c0591efabc6c857d
Rémai Gábor <remaigabor@gmail.com>
2022-11-23 14:12:03 +0100

    Update update-config.sh

--- update-config.sh
+++ update-config.sh
@@ -5,17 +5,16 @@ See README.md file for details how to configure"
 
 
 # Download dockerfiles
-if [ ${LOCALDEVMODE} == "1" ]; then  
+if [ ${LOCALDEVMODE} != 0  ]; then  
   # dev-dockerfile
-  curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETCLIENT}/${BRANCHCLIENT}/Dockerfile-dev" -o "Dockefile-client" && curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETSERVER}/${BRANCHSERVER}/Dockerfile-dev" -o "Dockefile-server"
+  curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETCLIENT}/${BRANCHCLIENT}/Dockerfile-dev" -o "Dockerfile-client" && curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETSERVER}/${BRANCHSERVER}/Dockerfile-dev" -o "Dockerfile-server"
 else
   # production dockerfile
-  curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETCLIENT}/${BRANCHCLIENT}/Dockerfile" -o "Dockefile-client" && curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETSERVER}/${BRANCHSERVER}/Dockerfile" -o "Dockefile-server"
+  curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETCLIENT}/${BRANCHCLIENT}/Dockerfile" -o "Dockerfile-client" && curl -s "https://raw.githubusercontent.com/${REPOSITORY}/${EDUMEETSERVER}/${BRANCHSERVER}/Dockerfile" -o "Dockerfile-server"
 fi
 
 # client example config
 curl -s "https://raw.githubusercontent.com/REPOSITORY}/${EDUMEETCLIENT}/${BRANCHCLIENT}/public/config/config.example.js" -o "configs/app/config.example.js"
-
 # server example config(s)
 curl -s "https://raw.githubusercontent.com/REPOSITORY}/${EDUMEETSERVER}/${BRANCHSERVER}/server/config/config.example.js" -o "configs/server/config.example.js"
 # yaml for 4.0
