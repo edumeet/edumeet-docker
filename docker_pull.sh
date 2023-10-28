4619ea998488670b3284b26a42806d85efb4173a
Rémai Gábor <remaigabor@gmail.com>
2023-10-28 16:59:24 +0200

    Update docker_pull.sh

--- docker_pull.sh
+++ docker_pull.sh
@@ -11,16 +11,28 @@ ${GREEN}To pull nightly docker images:${NOCOLOR}
 ${NOCOLOR}
 
 docker pull edumeet/${EDUMEET_MN_SERVER}:${TAG}
-docker pull edumeet/${EDUMEET_CLIENT}:${TAG}
+docker pull edumeet/${EDUMEET_MGMT_SERVER}:${TAG}
+docker pull edumeet/${EDUMEET_MGMT_CLIENT}:${TAG}
 docker pull edumeet/${EDUMEET_SERVER}:${TAG}
+docker pull edumeet/${EDUMEET_CLIENT}:${TAG}
 "
 
 if [ "$1" == "pull" ]; then     ## GOOD
     echo -e "RUNING COMMANDS:
     "
     docker pull edumeet/${EDUMEET_MN_SERVER}:${TAG}
-    docker pull edumeet/${EDUMEET_CLIENT}:${TAG}
+    docker pull edumeet/${EDUMEET_MGMT_SERVER}:${TAG}
+    docker pull edumeet/${EDUMEET_MGMT_CLIENT}:${TAG}
     docker pull edumeet/${EDUMEET_SERVER}:${TAG}
+    docker pull edumeet/${EDUMEET_CLIENT}:${TAG}
+elif [ "$1" == "push" ]; then     ## GOOD
+    echo -e "RUNING COMMANDS:
+    "
+    docker push edumeet/${EDUMEET_MN_SERVER}:${TAG}
+    docker push edumeet/${EDUMEET_MGMT_SERVER}:${TAG}
+    docker push edumeet/${EDUMEET_MGMT_CLIENT}:${TAG}
+    docker push edumeet/${EDUMEET_SERVER}:${TAG}
+    docker push edumeet/${EDUMEET_CLIENT}:${TAG}
 
 else
     echo -e "${RED}or run ./docker_pull.sh pull
