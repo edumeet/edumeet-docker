4aebe3809d8c84653de89b684264017ea5070072
Rémai Gábor <remaigabor@gmail.com>
2025-02-18 16:12:44 +0100

    4.1 (#62)
    * remove mgmt-client
    
    * remove mgmt-client from proxy
    
    * remove turn udp and mgmt-client config
    
    * add key+secret generation to run-me-first
    
    * use env for mgmt-public key
    
    * update startup script
    
    * remove mgmt-client

--- configs/mgmt-client/config.example.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line
-var config = {
-    serverApiUrl: "https://edumeet.example.com/mgmt",
-    hostname: "https://edumeet.example.com",
-    path: "/mgmt/socket.io",
-    port: "3030",
-    clipath: "/cli/"
-};
---
+++ create-cert.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+openssl genrsa -out "rsa_4096_priv.pem" 4096
+openssl rsa -in "rsa_4096_priv.pem" -pubout -out "rsa_4096_pub.pem"
